667ea03942e82b30dbe29e7290289baa016e4134
Eashwarsai,boini <20bd1a056b@gmail.com>
2022-11-16 12:09:23 +0530

    fine

---
+++ src/main/java/in/kmit/ExamMaven/App.java
@@ -0,0 +1,13 @@
+package in.kmit.ExamMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is se lab internal 1 exam" );
+    }
+}
